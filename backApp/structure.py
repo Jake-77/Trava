258f77c4a8bf26914d1878fa69a4b99129f1c514
Jake-77 <jake.lambkin@rogers.com>
2025-10-08 16:09:56 -0400

    Made some setup adjustments

--- backApp/structure.py
+++ backApp/structure.py
@@ -5,6 +5,21 @@ from flask import request, redirect, url_for, session
 import os
 import time
 
+#Always create the a virtual enviorment in the backApp directory to run code
+
+"""
+  cd backApp
+  python -m venv venv
+  .\venv\Scripts\Activate.ps1
+  pip install -r requirements.txt
+
+  two terminals in do
+    yarn start
+  the other do
+    yarn start-app
+
+"""
+
 app = Flask(__name__)
 
 @app.route("/")
--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+/*
+  cd backApp
+  python -m venv venv
+  .\venv\Scripts\Activate.ps1
+  pip install -r requirements.txt
+
+  two terminals in do
+    yarn start
+  the other do
+    yarn start-app
+
+*/
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    fetch('/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }, []);
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +39,7 @@ function App() {
         >
           Learn React
         </a>
+        <p>The current time is {currentTime}.</p>
       </header>
     </div>
   );
