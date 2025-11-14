6b849edc0546695f907f74c9239f05f1a4021971
Miles <miles.purvis@vidyard.com>
2025-11-14 11:52:32 -0500

    instructions

--- backApp/structure.py
+++ backApp/structure.py
@@ -2,6 +2,20 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 import time
 
+
+#Always create the a virtual enviorment in the backApp directory to run code
+
+"""
+  cd backApp
+  python -m venv venv
+  .\venv\Scripts\Activate.ps1
+  pip install -r requirements.txt
+  two terminals in do
+    npm run start
+  the other do
+    npm run start-app
+"""
+
 app = Flask(__name__)
 CORS(app)
 
@@ -30,4 +44,5 @@ def create_service():
     }
 
     services_db.append(service)
+    print("Service created:", service)
     return jsonify(service), 201
